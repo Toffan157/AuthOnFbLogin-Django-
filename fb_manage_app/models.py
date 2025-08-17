86c65bb697fa74473ba4aa09572788764b05c7ee
Toffan157 <mahaliktoffan1999@gmail.com>
2025-08-17 18:26:55 +0530

    modified Models file

--- fb_manage_app/models.py
+++ fb_manage_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class FacebookPage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    page_id = models.CharField(max_length=255)
+    page_name = models.CharField(max_length=255)
+    access_token = models.TextField()
+    permissions = models.JSONField(default=list)
+    created_at = models.DateTimeField(auto_now_add=True)    
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.page_name
